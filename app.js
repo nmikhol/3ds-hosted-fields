70f43c95e989c9a78edd351db32efced70650eb4
nmikhol <nataliemikhol@gmail.com>
2019-10-29 17:55:01 -0500

    generate client token

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const braintree = require("braintree")
+const path = require('path')
+const exphbs = require('express-handlebars')
+const gateway = require('./btCredentials.js')
+
+// establish port
+const PORT = process.env.PORT || 3000
+const app = express()
+
+app.set('views', path.join(__dirname, 'views')) // specify that templates will live in the "views" directory
+app.engine('.hbs', exphbs({extname: '.hbs'}))
+app.set('view engine', '.hbs') // specify that we are using "handlebars" as our template engine
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.listen(PORT, () => console.log(`App is up and running listening on port ${PORT}`))
+
+app.get('/', (req, res, next) => {
+  gateway.clientToken.generate({}, function (err, response) {
+    res.render('home', {clientToken: response.clientToken}, console.log(response.clientToken))
+})
+
+})
--- btCredentials.js
+++ btCredentials.js
@@ -1,10 +1,15 @@
 require('dotenv').config()
-let braintree = require("braintree");
+const braintree = require("braintree");
+const express = require('express');
+const router = express.Router();
 
 
-let gateway = braintree.connect({
-  environment: braintree.Environment.Sandbox,
-  merchantId: process.env.BT_MERCHANT_ID,
-  publicKey: process.env.BT_PUBLIC_KEY,
-  privateKey: process.env.BT_PRIVATE_KEY
-});
+  let gateway = braintree.connect({
+    environment: braintree.Environment.Sandbox,
+    merchantId: process.env.BT_MERCHANT_ID,
+    publicKey: process.env.BT_PUBLIC_KEY,
+    privateKey: process.env.BT_PRIVATE_KEY
+  })
+
+
+module.exports = gateway
