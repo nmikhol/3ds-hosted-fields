ef87595f91a53e7eb0ec805e8b10ed034bf558c8
nmikhol <nataliemikhol@gmail.com>
2019-10-29 07:41:42 -0500

    install packages and input credentials

---
+++ btCredentials.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+let braintree = require("braintree");
+
+
+let gateway = braintree.connect({
+  environment: braintree.Environment.Sandbox,
+  merchantId: process.env.BT_MERCHANT_ID,
+  publicKey: process.env.BT_PUBLIC_KEY,
+  privateKey: process.env.BT_PRIVATE_KEY
+});
